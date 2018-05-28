8082e07c51b40644aa63f6962fd0f6619cda7ad9
王飞龙 <wangfeilong@raycloud.com>
2018-05-28 22:11:07 +0800

    api path

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(session({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter, signRouter, usersRouter);
+app.use('/api', indexRouter, signRouter, usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
